0cf63b9047bfe993ae7142a8fe3dc11a632591fd
Edward-uri <233290@ids.upchiapas.edu.mx>
2026-03-10 19:28:22 -0600

    [FEAT] Se agrego al Modulo de Hardware la nueva funcion de la camara y se hizo el bindeo

--- app/src/main/java/com/rentitem/core/di/HardwareModule.kt
+++ app/src/main/java/com/rentitem/core/di/HardwareModule.kt
@@ -1,12 +1,22 @@
 package com.rentitem.core.di
 
 import android.content.Context
+import com.rentitem.core.hardware.data.AndroidCameraManager
 import com.rentitem.core.hardware.data.AndroidGpsManager
+import com.rentitem.core.hardware.domain.CameraManager
 import com.rentitem.core.hardware.domain.GpsManager
+import dagger.Binds
+import javax.inject.Singleton
 
-class HardwareModule(private val context: Context) {
+abstract class HardwareModule(private val context: Context) {
 
     val gpsManager: GpsManager by lazy {
         AndroidGpsManager(context)
     }
+
+    @Binds
+    @Singleton
+    abstract fun bindCameraManager(
+        impl: AndroidCameraManager
+    ): CameraManager
 }
